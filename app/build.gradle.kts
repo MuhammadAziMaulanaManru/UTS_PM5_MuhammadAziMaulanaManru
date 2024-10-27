56edeb1f9c24161a193ef93d8ffdfc480918b3c2
muhammadazi <muhammadazi150924@gmail.com>
2024-10-27 22:12:09 +0700

    menambahkan color, font, dan string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.project_uts"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
