6501e87f356d3876e173d0d60d60d8f5984407db
muhammadazi <muhammadazi150924@gmail.com>
2024-10-30 23:19:37 +0700

    menyelesaikan code untuk quiz two dan quiz three

---
+++ app/src/main/java/com/example/project_uts/QuizPageTwo.kt
@@ -0,0 +1,41 @@
+package com.example.project_uts
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuizPageTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_quiz_page_two)
+        val btnOpsiPertama: Button = findViewById(R.id.btn_opsi_pertama)
+        val btnOpsiKedua: Button = findViewById(R.id.btn_opsi_kedua)
+        val btnOpsiKetiga: Button = findViewById(R.id.btn_opsi_ketiga) // Correct Answer
+        val btnOpsiKeempat: Button = findViewById(R.id.btn_opsi_keempat)
+        val btnNext: Button = findViewById(R.id.btn_next)
+
+        // Set listeners for each button
+        btnOpsiPertama.setOnClickListener { sendAnswer("Wrong Answer!") }
+        btnOpsiKedua.setOnClickListener { sendAnswer("Correct Answer!") }
+        btnOpsiKetiga.setOnClickListener { sendAnswer("Wrong Answer!") } // Correct Option
+        btnOpsiKeempat.setOnClickListener { sendAnswer("Wrong Answer!") }
+        btnNext.setOnClickListener { sendAnswer("Wrong Answer!") }
+    }
+
+    private fun sendAnswer(answer: String) {
+        val answerResult1 = intent.getStringExtra("ANSWER_RESULT_1")
+        val name = intent.getStringExtra("name")
+
+        val intent = Intent(this, QuizPageThree::class.java)
+        intent.putExtra("ANSWER_RESULT_1", answerResult1)
+        intent.putExtra("ANSWER_RESULT_2", answer)
+        intent.putExtra("name",name)
+        startActivity(intent)
+    }
+}
