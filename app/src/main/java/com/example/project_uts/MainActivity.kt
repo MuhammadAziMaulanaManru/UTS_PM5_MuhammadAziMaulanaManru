f58c037698a60cd08fc2ef0da1327f089f4e49e4
muhammadazi <muhammadazi150924@gmail.com>
2024-10-30 23:21:02 +0700

    menyelesaikan code untuk main activity, finish page, dan gradle serta android manifest

--- app/src/main/java/com/example/project_uts/MainActivity.kt
+++ app/src/main/java/com/example/project_uts/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.project_uts
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +14,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_finish_page)
+
+        val name = intent.getStringExtra("name")
+
+        var totalScore = 0
+
+        for (i in 1..5) {
+            val userAnswer = intent.getStringExtra("ANSWER_RESULT_$i")
+
+            // Cek apakah jawaban benar
+            if (userAnswer == "Correct Answer!") {
+                totalScore += 20 // Tambah 20 poin untuk setiap jawaban benar
+            }
+
+        }
+        val tvNilai: Button = findViewById(R.id.tv_nilai)
+        val tvName: TextView = findViewById(R.id.tv_nama)
+        val btFinish: Button = findViewById(R.id.bt_finish)
+
+
+        // Set nilai skor ke Button tv_nilai
+        tvNilai.text = totalScore.toString()
+        tvName.text = name.toString()
+
+        // Set click listener untuk tombol finish (opsional)
+        btFinish.setOnClickListener {
+            Toast.makeText(this, "Selesai! Skor Anda: $totalScore", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, OnBoardingPageOne::class.java)
+            startActivity(intent)
+            finish() // Menutup activity
         }
     }
-}
+    }
